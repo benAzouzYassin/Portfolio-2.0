306f715c25f54d9b2f79b4504ed7c68905059721
yassine ben azouz <yassinebenazouz123@gmail.com>
2023-09-28 23:36:36 +0200

    made the projects links work

--- components/Projects/Project.tsx
+++ components/Projects/Project.tsx
@@ -24,12 +24,14 @@ export default function Project(props: Props) {
 
   return (
     <div ref={ref} className={`opacity-0 text-[#aeb8d3] `}>
-      <img
-        className=" aspect-[16/9]   rounded-md hover:cursor-pointer hover:scale-105 transition-transform  "
-        src={props.img}
-        alt={props.description}
-        loading="lazy"
-      />
+      <a href={props.link} target="_blank">
+        <img
+          className=" aspect-[16/9]   rounded-md hover:cursor-pointer hover:scale-105 transition-transform  "
+          src={props.img}
+          alt={props.description}
+          loading="lazy"
+        />
+      </a>
       <h4 className="lg:text-xl text-base mt-3 font-medium">{props.name}</h4>
       <p className="font-normal mt-1 lg:block hidden">{props.description}</p>
     </div>
