1a2a4d415e74d0af60315afd1cf564bd91fe3ff0
yassine ben azouz <yassinebenazouz123@gmail.com>
2024-01-05 19:33:27 +0100

    Update TopArrow.tsx

--- components/TopArrow/TopArrow.tsx
+++ components/TopArrow/TopArrow.tsx
@@ -41,7 +41,7 @@ export default function TopArrow() {
         <div
           ref={ref}
           onClick={handleClick}
-          className={` w-10 h-10 right-5 bg-white  hover:cursor-pointer hover:scale-110  top-[93vh] flex items-center justify-center fixed transition-all  rounded-full ${styles["btn-animation"]}`}
+          className={` z-50 w-10 h-10 right-5 bg-white  hover:cursor-pointer hover:scale-110  top-[93vh] flex items-center justify-center fixed transition-all  rounded-full ${styles["btn-animation"]}`}
         >
           <ArrowUpIcon className="scale-105 " />
         </div>
