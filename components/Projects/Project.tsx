d57bc36fd3a6ceb78a9b54768ba1cab265925d58
benAzouzYassin <yassinebenazouz123@gmail.com>
2024-01-09 10:13:57 +0100

    fixing projects issue

--- components/Projects/Project.tsx
+++ components/Projects/Project.tsx
@@ -16,27 +16,30 @@ export default function Project(props: Props) {
   const animate = useAnimation()
   const ref = useRef(null)
   const inView = useInView(ref, { margin: "-200px" })
-  // const even = props.index % 2 === 0
-  // const translateX = even? -100 : 100
+  const even = props.index % 2 === 0
+  const translateX = even ? -100 : 100
   useEffect(() => {
     if (inView) {
       animate.start({ opacity: 1, x: 0 })
     }
   }, [inView])
-  return (
-    <motion.div animate={animate} transition={{ duration: 0.8 }} initial={{ opacity: 0, x: -  100 }} ref={ref} className="text-[#aeb8d3] lg:w-auto w-[80vw] ">
-      <a href={props.link} target="_blank">
-        <Image
-          width={750}
-          height={700}
-          className="aspect-[16/9] hover:rotate-1 hover:scale-[102%] rounded-md hover:cursor-pointer transition-transform  "
-          src={props.img}
-          alt={props.description}
-          loading="lazy"
-        />
-      </a>
-      <p className="lg:text-xl text-base mt-3 font-medium">{props.name}</p>
-      <p className="font-normal mt-1 lg:block hidden w-[95%]">{props.description}</p>
-    </motion.div>
+  return (<>
+      <div className="h-2 w-2 " ref={ref}></div >
+        <motion.div animate={animate} transition={{ duration: 0.8 }} initial={{ opacity: 0, x: translateX }} className="text-[#aeb8d3] lg:w-auto w-[80vw] ">
+          <a href={props.link} target="_blank">
+            <Image
+              quality={100}
+              width={750}
+              height={700}
+              className="aspect-[16/9] hover:rotate-1 hover:scale-[102%] rounded-md hover:cursor-pointer transition-transform  "
+              src={props.img}
+              alt={props.description}
+              loading="lazy"
+            />
+          </a>
+          <p className="lg:text-xl text-base mt-3 font-medium">{props.name}</p>
+          <p className="font-normal mt-1 lg:block hidden w-[95%]">{props.description}</p>
+        </motion.div>
+      </>
   );
 }
