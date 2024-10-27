2be96dd723771ecfd20543625b8867eb8634bec1
yassinebenazouz123 <yassinebenazouz123@gmail.com>
2024-10-27 15:22:37 +0100

    minor changes

--- components/Experience/Experiences.tsx
+++ components/Experience/Experiences.tsx
@@ -1,47 +1,53 @@
-"use client"
+"use client";
 import { Inter } from "next/font/google";
 import experinces from "@/experiences.json";
 import Exp from "./Exp";
 import { ArrowRight } from "lucide-react";
 import { useSectionsContext } from "@/context/sectionsContext";
 import { useEffect, useRef } from "react";
-import { motion, useAnimation, useInView } from "framer-motion"
+import { motion, useAnimation, useInView } from "framer-motion";
+import Link from "next/link";
 
 const inter = Inter({ weight: "700", subsets: ["latin"] });
 
 export default function Experiences() {
-  const ref = useRef(null)
-  const inView = useInView(ref, { margin: "-100px" });
-  const { updateSection } = useSectionsContext()
-  const animate = useAnimation()
-  useEffect(() => {
-    if (inView) {
-      updateSection("EXPERIENCES")
-      animate.start({ opacity: 1, rotateX: 0, scale: 1 })
-    } else {
-      animate.start({ opacity: 0, rotateX: -80, scale: 0.9 })
-    }
-  }, [animate, inView, updateSection])
-  return (
-    <section id="EXPERIENCES" className="mt-10  text-[#cad6f6]">
-      <h2
-        className={`flex gap-1 mt-12 font-bold text-4xl text-[#cad6f6]  ${inter.className}`}
-      >
-        Experiences
-      </h2>
-      <div className="mt-10" ref={ref}>
-        {experinces.map((exp) => (
-          <motion.div animate={animate} initial={{ opacity: 0, rotateX: -80, scale: 0.9 }} transition={{ duration: 0.8 }} key={exp.name}><Exp  {...exp} /></motion.div>
-        ))}
-      </div>
-      <a
-        target="_blank"
-        href="https://drive.google.com/file/d/1pk0GUwhBXiZ1KNxPMRFAVcSTMSncvPyO/view"
-        className="flex lg:text-lg text-sm ml-2  mt-10 font-normal lg:no-underline underline gap-1 underline-offset-[6px] hover:underline hover:gap-3 transition-all  hover:text-[#59e5c4] "
-      >
-        Feel free to review my professional Resume.{" "}
-          <ArrowRight className=" lg:block hidden  self-end scale-105 my-auto "/>{" "}
-      </a>
-    </section>
-  );
+    const ref = useRef(null);
+    const inView = useInView(ref, { margin: "-100px" });
+    const { updateSection } = useSectionsContext();
+    const animate = useAnimation();
+    useEffect(() => {
+        if (inView) {
+            updateSection("EXPERIENCES");
+            animate.start({ opacity: 1, rotateX: 0, scale: 1 });
+        } else {
+            animate.start({ opacity: 0, rotateX: -80, scale: 0.9 });
+        }
+    }, [animate, inView, updateSection]);
+    return (
+        <section id="EXPERIENCES" className="mt-10  text-[#cad6f6]">
+            <h2
+                className={`flex gap-1 mt-12 font-bold text-4xl text-[#cad6f6]  ${inter.className}`}>
+                Experiences
+            </h2>
+            <div className="mt-10" ref={ref}>
+                {experinces.map((exp) => (
+                    <motion.div
+                        animate={animate}
+                        initial={{ opacity: 0, rotateX: -80, scale: 0.9 }}
+                        transition={{ duration: 0.8 }}
+                        key={exp.name}>
+                        <Exp {...exp} />
+                    </motion.div>
+                ))}
+            </div>
+            <Link
+                prefetch
+                target="_blank"
+                href="/cv.pdf"
+                className="flex lg:text-lg text-sm ml-2  mt-10 font-normal lg:no-underline underline gap-1 underline-offset-[6px] hover:underline hover:gap-3 transition-all  hover:text-[#59e5c4] ">
+                Feel free to review my professional Resume.{" "}
+                <ArrowRight className=" lg:block hidden  self-end scale-105 my-auto " />{" "}
+            </Link>
+        </section>
+    );
 }
--- components/FixedInfo/FixedInfo.tsx
+++ components/FixedInfo/FixedInfo.tsx
@@ -2,104 +2,100 @@ import { Roboto, Space_Grotesk } from "next/font/google";
 import style from "./styles.module.css";
 import Timer from "./Timer";
 import Nav from "./Nav";
+import Link from "next/link";
 const roboto = Roboto({
-  subsets: ["cyrillic-ext"],
-  weight: ["500", "400", "300"],
+    subsets: ["cyrillic-ext"],
+    weight: ["500", "400", "300"],
 });
 const space_Grotesk = Space_Grotesk({
-  subsets: ["latin"],
-  weight: ["700", "500"],
+    subsets: ["latin"],
+    weight: ["700", "500"],
 });
 export default function FixedInfo() {
-  const currentDate = new Date();
-  return (
-    <div className="lg:w-1/2 lg:fixed  ">
-      <svg
-        className=" text-white overflow-visible"
-        viewBox="0 0 634 97"
-        width="300"
-        height="100"
-      >
-        <text
-          id="animated-text"
-          className={`${style["animated-text"]} text-[200px]  ${roboto.className} font-medium`}
-          x="0%"
-          y="80%"
-          textAnchor="left"
-          fill="none"
-        >
-          Yassine
-        </text>
-        <use
-          xlinkHref="#animated-text"
-          className={style["animated-text-copy"]}
-        ></use>
-      </svg>
-      <h1
-        className={` pl-4 text-lg  uppercase tracking-[0.4rem] text-slate-400 sm:text-xl ${space_Grotesk.className}`}
-      >
-        Software Developer
-      </h1>
-      <div className="mt-4 flex gap-4 pl-4">
-        <a
-          href="https://github.com/benAzouzYassin"
-          target="_blank"
-          className="hover:scale-110 text-md font-medium"
-        >
-          <img
-            alt="GitHub"
-            height="20"
-            width="20"
-            src="https://cdn.simpleicons.org/GitHub/white"
-          />
-        </a>
-        <a
-          href="https://www.facebook.com/yassine.benazouz.18/"
-          target="_blank"
-          className=" hover:scale-110 text-md font-medium"
-        >
-          <img
-            alt="Facebook"
-            height="20"
-            width="20"
-            src="https://cdn.simpleicons.org/Facebook/white"
-          />
-        </a>
+    const currentDate = new Date();
+    return (
+        <div className="lg:w-1/2 lg:fixed  ">
+            <svg
+                className=" text-white overflow-visible"
+                viewBox="0 0 634 97"
+                width="300"
+                height="100">
+                <text
+                    id="animated-text"
+                    className={`${style["animated-text"]} text-[200px]  ${roboto.className} font-medium`}
+                    x="0%"
+                    y="80%"
+                    textAnchor="left"
+                    fill="none">
+                    Yassine
+                </text>
+                <use
+                    xlinkHref="#animated-text"
+                    className={style["animated-text-copy"]}></use>
+            </svg>
+            <h1
+                className={` pl-4 text-lg  uppercase tracking-[0.4rem] text-slate-400 sm:text-xl ${space_Grotesk.className}`}>
+                Software Developer
+            </h1>
+            <div className="mt-4 flex gap-4 pl-4">
+                <a
+                    href="https://github.com/benAzouzYassin"
+                    target="_blank"
+                    className="hover:scale-110 text-md font-medium">
+                    <img
+                        alt="GitHub"
+                        height="20"
+                        width="20"
+                        src="https://cdn.simpleicons.org/GitHub/white"
+                    />
+                </a>
+                <a
+                    href="https://www.facebook.com/yassine.benazouz.18/"
+                    target="_blank"
+                    className=" hover:scale-110 text-md font-medium">
+                    <img
+                        alt="Facebook"
+                        height="20"
+                        width="20"
+                        src="https://cdn.simpleicons.org/Facebook/white"
+                    />
+                </a>
 
-        <a
-          href="https://www.linkedin.com/in/yassine-ben-azouz123"
-          target="_blank"
-          className="hover:scale-110 text-md font-medium"
-        >
-          <img
-            alt="LinkedIn"
-            height="20"
-            width="20"
-            src="https://cdn.simpleicons.org/LinkedIn/white"
-          />
-        </a>
-        <a
-          href="mailto:yassinebenazouz123@gmail.com"
-          target="_blank"
-          className="hover:scale-110 text-md font-medium"
-        >
-          <img
-            alt="Gmail"
-            height="20"
-            width="20"
-            src="https://cdn.simpleicons.org/Gmail/white"
-          />
-        </a>
-      </div>
-      <a target="_blank" href="mailto:yassinebenazouz123@gmail.com" className="mt-8 ml-4  flex w-[300px] items-center gap-3 rounded-full bg-gradient-to-r from-[#122D22] via-[#265842] to-[#122D22] p-2 lg:px-4">
-        <div className="flex h-5  w-5 items-center justify-center rounded-full bg-[#3ECF8E26]">
-          <div className="m-auto h-3 w-3 animate-ping rounded-full bg-[rgb(62,207,142)]"></div>
+                <a
+                    href="https://www.linkedin.com/in/yassine-ben-azouz123"
+                    target="_blank"
+                    className="hover:scale-110 text-md font-medium">
+                    <img
+                        alt="LinkedIn"
+                        height="20"
+                        width="20"
+                        src="https://cdn.simpleicons.org/LinkedIn/white"
+                    />
+                </a>
+                <a
+                    href="mailto:yassinebenazouz123@gmail.com"
+                    target="_blank"
+                    className="hover:scale-110 text-md font-medium">
+                    <img
+                        alt="Gmail"
+                        height="20"
+                        width="20"
+                        src="https://cdn.simpleicons.org/Gmail/white"
+                    />
+                </a>
+            </div>
+            <Link
+                target="_blank"
+                href="/cv.pdf"
+                className="mt-8 ml-4  flex w-[300px] items-center gap-3 rounded-full bg-gradient-to-r from-[#122D22] via-[#265842] to-[#122D22] p-2 lg:px-4">
+                <div className="flex h-5  w-5 items-center justify-center rounded-full bg-[#3ECF8E26]">
+                    <div className="m-auto h-3 w-3 animate-ping rounded-full bg-[rgb(62,207,142)]"></div>
+                </div>
+                <span className="text-[#3ECF8E]">Available for projects</span>
+            </Link>
+            <Timer currentDate={currentDate} />
+            <div className="mt-6 ml-4  h-[2px] w-3/4 rounded-full bg-gradient-to-r from-[#3ECF8E]  to-[#0C1311] "></div>
+            <Nav />
         </div>
-        <span className="text-[#3ECF8E]">Available for projects</span>
-      </a>
-      <Timer currentDate={currentDate} />
-      <div className="mt-6 ml-4  h-[2px] w-3/4 rounded-full bg-gradient-to-r from-[#3ECF8E]  to-[#0C1311] "></div>
-      <Nav />
-    </div>
-  );
+    );
 }
